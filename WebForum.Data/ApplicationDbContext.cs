2bc531b39e361477b9c3e100f3beface4222665a
Amer Hasanbegovic <amer.hasanbegovic@hotmail.com>
2020-09-22 23:01:34 +0200

    Initial commit

---
+++ WebForum.Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebForum.Data.Models;
+
+namespace WebForum.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ApplicationUser> AplicationUser { get; set; }
+        public DbSet<Forum> Forums { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostReply> PostReplies { get; set; }
+    }
+}
---
+++ WebForum.Data/IForum.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WebForum.Data
+{
+    public interface IForum
+    {
+    }
+}
