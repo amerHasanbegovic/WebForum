d661e1ed6968b1ba3eae7fe7719e23ee73e47a4f
Amer Hasanbegovic <amer.hasanbegovic@hotmail.com>
2020-09-24 00:27:50 +0200

    added post controller, post reply model

---
+++ WebForum/Controllers/PostController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebForum.Data;
+
+namespace WebForum.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly IPost _postService;
+        public PostController(IPost postService)
+        {
+            _postService = postService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
