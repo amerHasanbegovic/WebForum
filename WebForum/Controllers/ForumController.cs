525e45bbf7a0a362858e7ca536d36e45295067ca
Amer Hasanbegovic <amer.hasanbegovic@hotmail.com>
2020-09-25 16:43:40 +0200

    ApplicationDbContext fix

--- WebForum/Controllers/ForumController.cs
+++ WebForum/Controllers/ForumController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
-using Microsoft.Extensions.DependencyInjection;
 using WebForum.Data;
 using WebForum.Data.Models;
 using WebForum.Models.Forum;
@@ -41,15 +38,17 @@ namespace WebForum.Controllers
         {
             var forum = _forumService.GetById(id);
             var posts = _postService.GetPostsByForum(id);
+            //var posts = _postService.GetPostsByForum(id);
             var postListings = posts.Select(post => new PostListingModel
             {
                 Id = post.Id,
+                Forum = BuildForumListing(post),
                 AuthorId = post.User.Id,
+                AuthorName = post.User.UserName,
                 AuthorRating = post.User.Rating,
                 Title = post.Title,
                 DatePosted = post.dateTime.ToString(),
-                RepliesCount = post.Replies.Count(),
-                Forum = BuildForumListing(post)
+                RepliesCount = post.Replies.Count()
             });
 
             var model = new ForumTopicModel
